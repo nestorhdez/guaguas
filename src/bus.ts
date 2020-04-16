3047f7b7db8fe99383fb0cd312f782fe0a6cb62f
Nestor hdez <nestorhdez.26@gmail.com>
2020-04-16 19:31:38 +0100

    Initial commit

---
+++ src/bus.ts
@@ -0,0 +1,64 @@
+import { Context } from "aws-lambda";
+import { Page, Browser } from "puppeteer";
+
+const chromium = require('chrome-aws-lambda');
+
+type LineData = {line: string, time: string};
+
+exports.handler = async (event: any, context: Context) => {
+
+  const { busStop, line } = event.queryStringParameters;
+  
+  if(!busStop) {
+    return {
+      statusCode: 400,
+      body: 'busStop query needed'
+    }
+  }
+  console.log({busStop, line});
+
+  let busStopResponse: LineData[];
+
+  const browser: Browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  });
+  const page: Page = await browser.newPage();
+  await page.goto('https:///www.guaguas.com/');
+
+  try {
+    await page.click('#nav-tabs-wrapper > li.tab2 > a');
+    await page.type('#laparada_menu', (busStop as string));
+    await page.click('#consulta_parada_menu > div > button[type=submit]');
+    await page.waitFor('#resp_parada_menu > div > table', {visible: true});
+
+    const rowsSelector = '#resp_parada_menu > div > table > tbody > tr';
+
+    const response = await page.$$eval(rowsSelector, rows =>
+      rows.map( ({ children }: Element) => ({
+        line: children[0].textContent!,
+        time: children[2].textContent!.split(' ')[0]
+      }))
+    );
+
+    busStopResponse = line
+      ? response.filter( ({line: l} ) => l === line)
+      : response;
+
+  } catch(error) {
+    await browser.close();
+    return {
+      statusCode: 500,
+      body: error
+    };
+  }
+
+  await browser.close();
+  
+  return {
+    statusCode: 200,
+    body: busStopResponse
+  };
+};
