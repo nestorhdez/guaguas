0cf00cbdd9eead3471d3c205bd621477a9be4bd7
Nestor hdez <nestorhdez.26@gmail.com>
2020-04-25 12:32:20 +0100

    Adding puppeteer code

--- bus/bus.js
+++ bus/bus.js
@@ -1,20 +1,66 @@
-const axios = require("axios");
+const chromium = require('chrome-aws-lambda');
+
+const { args, defaultViewport, executablePath, headless, puppeteer } = chromium;
 
 exports.handler = async (event, context) => {
 
-  const { todo } = event.queryStringParameters;
+  const { busStop, line } = event.queryStringParameters;
 
-  if(!todo) {
+  let browser;
+  let busStopResponse;
+
+  if(!busStop) {
     return {
       statusCode: 400,
-      body: 'todo query needed'
+      body: 'busStop query needed'
     }
   }
 
-  const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todo}`);
+  try {
+    browser = await puppeteer.launch({
+      args,
+      defaultViewport,
+      executablePath: await executablePath,
+      headless,
+    });
+
+    const page = await browser.newPage();
+    await page.goto('https:///www.guaguas.com/');
+    await page.click('#nav-tabs-wrapper > li.tab2 > a');
+    await page.type('#laparada_menu', busStop);
+    await page.click('#consulta_parada_menu > div > button[type=submit]');
+    await page.waitFor('#resp_parada_menu > div > table', {visible: true});
+
+    const rowsSelector = '#resp_parada_menu > div > table > tbody > tr';
+
+    const response = await page.$$eval(rowsSelector, rows =>
+      rows.map( ({ children }) => ({
+        line: children[0].textContent,
+        time: children[2].textContent.split(' ')[0]
+      }))
+    );
+
+    busStopResponse = line
+      ? response.filter( ({line: l} ) => l === line)
+      : response;
+
+  } catch(error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error
+      })
+    };
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify(response.data)
+    body: JSON.stringify({
+      result: busStopResponse
+    })
   };
 };
