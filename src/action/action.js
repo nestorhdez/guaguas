37661c059bda048db3ea0e122b81a680f0ea2bcb
Nestor hdez <nestorhdez.26@gmail.com>
2020-04-28 19:48:56 +0100

    Adding helmet again

--- src/action/action.js
+++ src/action/action.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const serverless = require('serverless-http');
+const helmet = require('helmet');
 
 const app = express();
 const router = express.Router();
@@ -10,7 +11,7 @@ const router = express.Router();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.disable('x-powered-by');
+app.use(helmet());
 
 router.post('/', (request, response) => {
   response.json({res: 'Post received'});
