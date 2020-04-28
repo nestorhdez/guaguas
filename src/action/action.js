aae9e7b4087ddaced78875a59337d9725a070bc3
Nestor hdez <nestorhdez.26@gmail.com>
2020-04-28 17:25:19 +0100

    Use serverless and express

--- src/action/action.js
+++ src/action/action.js
@@ -1,14 +1,20 @@
-exports.handler = async (event, context) => {
-  
-  if(event.httpMethod !== 'POST') {
-    return {
-      statusCode: 400,
-      body: 'http method should be "post"'
-    } 
-  }
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-  return {
-    status: 200,
-    body: JSON.stringify(event.body)
-  }
-}
+let express = require('express');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+
+app.post('/', (request, response) => {
+  response.status(200).send('Post received');
+});
+
+exports.handler = serverless(app);
