cac55c6abec8e368c66e75bbdc65dd5969bced64
Nestor hdez <nestorhdez.26@gmail.com>
2020-04-28 18:07:51 +0100

    Using router and remove helmet

--- src/action/action.js
+++ src/action/action.js
@@ -2,18 +2,20 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
-const helmet = require('helmet');
 const serverless = require('serverless-http');
 
 const app = express();
+const router = express.Router();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
+app.disable('x-powered-by');
 
-app.post('/', (request, response) => {
+router.post('/', (request, response) => {
   response.json({res: 'Post received'});
 });
+
+app.use('/.netlify/functions/action', router);
 
 exports.handler = serverless(app);
